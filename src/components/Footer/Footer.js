ed85e45a6692a1002bf6cd5e127fb80654cacc87
santanaruben <rubensantana@outlook.com>
2021-06-10 14:39:52 -0400

    footer link fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,90 +1,124 @@
-import React from 'react';
-import { Button } from '../../globalStyles';
+import React from "react";
+import { Button } from "../../globalStyles";
 import {
   FaFacebook,
   FaInstagram,
   FaYoutube,
   FaTwitter,
-  FaLinkedin
-} from 'react-icons/fa';
+  FaLinkedin,
+} from "react-icons/fa";
 import {
-    FooterContainer,
-    FooterSubText,
-    FooterSubHeading,
-    Form,
-    FormInput,
-    FooterLinksContainer,
-    FooterLinksWrapper,
-    FooterLinkItems,
-    FooterLinkTitle,
-    FooterLink,
-    SocialMedia,
-    SocialMediaWrap,
-    SocialLogo,
-    SocialIcon,
-    WebsiteRights,
-    SocialIcons,
-    SocialIconLink
-  } from './Footer.elements';
+  FooterContainer,
+  FooterSubText,
+  FooterSubHeading,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+  FooterLinkURL,
+} from "./Footer.elements";
 
-const logoImage = require('../../images/logo.png');
+const logoImage = require("../../images/logo.png");
 
-  const Footer = () => {
-    return (
-        <FooterContainer>
-            <Form>
-              <FormInput name="email" type="email" placeholder="Your Email"/>
-                <Button fontBig>Contact Me</Button>            
-            </Form>
-          <FooterLinksContainer>
-            <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to='/sign-up'> How it works</FooterLink>
-                <FooterLink to='/mission'> Mission</FooterLink>
-                <FooterLink to='/terms'> Terms of Service</FooterLink>                
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>Contact Us</FooterLinkTitle>
-                <FooterLink to='/contact'> Contact</FooterLink>                
-              </FooterLinkItems>
-              </FooterLinksWrapper>
-              <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>Virtual Tour Course</FooterLinkTitle>
-                <FooterLink to={'https://www.youtube.com/channel/UCQfyVpZb8N3j3UFxsPsQwBw'}> Youtube</FooterLink>
-                <FooterLink to={'https://www.udemy.com/course/how-to-make-virtual-tours/learn/'}> Udemy</FooterLink>               
-              </FooterLinkItems>
-            </FooterLinksWrapper>
-          </FooterLinksContainer>
-          <SocialMedia>
-            <SocialMediaWrap>
-              <SocialLogo to="/">
-                <SocialIcon src={logoImage.default}/>
-                DBI360
-              </SocialLogo>
-              <WebsiteRights> DBI360 © 2021</WebsiteRights>
-              <SocialIcons>
-                <SocialIconLink href={'https://www.facebook.com/DBI360VR'} target='_blank' aria-label='Facebook'>
-                  <FaFacebook/>
-                </SocialIconLink>
-                <SocialIconLink href={'https://www.instagram.com/dbi360ar/'} target='_blank' aria-label='Instagram'>
-                  <FaInstagram/>
-                </SocialIconLink>
-                <SocialIconLink href={'https://www.youtube.com/channel/UCQfyVpZb8N3j3UFxsPsQwBw'} rel='noopener noreferrer' target='_blank' aria-label='YouTube'>
-                  <FaYoutube/>
-                </SocialIconLink>
-                <SocialIconLink href={'https://twitter.com/dbi360ar?s=08'} target='_blank' aria-label='Twitter'>
-                  <FaTwitter/>
-                </SocialIconLink>
-                <SocialIconLink href={'https://www.linkedin.com/company/67896357'} target='_blank' aria-label='LinkedIn'>
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <Form>
+        <FormInput name="email" type="email" placeholder="Your Email" />
+        <Button fontBig>Contact Me</Button>
+      </Form>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to="/sign-up"> How it works</FooterLink>
+            <FooterLink to="/mission"> Mission</FooterLink>
+            <FooterLink to="/terms"> Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to="/contact"> Contact</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Virtual Tour Course</FooterLinkTitle>
+            <FooterLinkURL
+              href={"https://www.youtube.com/channel/UCQfyVpZb8N3j3UFxsPsQwBw"}
+              target="blank"
+            >
+              Youtube
+            </FooterLinkURL>
+            <FooterLinkURL
+              href={
+                "https://www.udemy.com/course/how-to-make-virtual-tours/learn/"
+              }
+              target="blank"
+            >
+              Udemy
+            </FooterLinkURL>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon src={logoImage.default} />
+            DBI360
+          </SocialLogo>
+          <WebsiteRights> DBI360 © 2021</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink
+              href={"https://www.facebook.com/DBI360VR"}
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.instagram.com/dbi360ar/"}
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.youtube.com/channel/UCQfyVpZb8N3j3UFxsPsQwBw"}
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="YouTube"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://twitter.com/dbi360ar?s=08"}
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.linkedin.com/company/67896357"}
+              target="_blank"
+              aria-label="LinkedIn"
+            >
               <FaLinkedin />
             </SocialIconLink>
-              </SocialIcons>
-            </SocialMediaWrap>
-          </SocialMedia>
-        </FooterContainer>
-    )
-}
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
