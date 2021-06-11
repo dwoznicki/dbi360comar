a143c647a9129d887ca12374413735137b9c3c92
santanaruben <rubensantana@outlook.com>
2021-06-11 13:27:22 -0400

    navbar link fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import GlobalStyle from './globalStyles';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import { Navbar, Footer} from './components';
-import Home from './pages/HomePage/Home';
-import Aboutus from './pages/Aboutus/Aboutus';
-import Prodviewer from './pages/Prodviewer/Prodviewer';
-import SignUp from './pages/SignUp/Signup';
-import Contactus from './pages/Contactus/Contactus';
-import ScrollToTop from './components/ScrollToTop'
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Footer } from "./components";
+import Home from "./pages/HomePage/Home";
+import Aboutus from "./pages/Aboutus/Aboutus";
+import SignUp from "./pages/SignUp/Signup";
+import Contactus from "./pages/Contactus/Contactus";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
@@ -18,12 +17,10 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/aboutus" exact component={Aboutus} />
-        <Route path="/prodviewer" exact component={Prodviewer} />
         <Route path="/contactus" exact component={Contactus} />
         <Route path="/sign-up" exact component={SignUp} />
       </Switch>
       <Footer />
-      
     </Router>
   );
 }
